ba9f4cbdf74ad86651f1b79eeb57a7bc7bb94dd7
Dhx27 <diogosilvalana27@gmail.com>
2024-12-27 20:23:05 -0300

    iniciando automação do Detran GO

---
+++ src/DetranGoias/detranGoias.py
@@ -0,0 +1,27 @@
+from dotenv import load_dotenv
+from openpyxl import load_workbook
+import time
+from selenium.webdriver.chrome.options import Options
+from selenium.webdriver.support.ui import WebDriverWait
+from selenium.webdriver.support import expected_conditions as EC
+from selenium.common.exceptions import TimeoutException, NoSuchElementException
+from selenium.webdriver.common.by import By
+from selenium.webdriver.support.ui import Select
+import undetected_chromedriver as uc
+import requests
+from selenium.webdriver.common.keys import Keys  # Para acessar as teclas especiais
+from selenium.webdriver.common.action_chains import ActionChains
+import os
+
+
+# Configurar o Chrome com um User-Agent falso usando undetected-chromedriver
+chrome_options = Options()
+chrome_options.add_argument("user-agent=Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/58.0.3029.110 Safari/537.36")
+
+# Inicializa o navegador com as opções e com o stealth mode ativado
+navegador = uc.Chrome(options=chrome_options)
+navegador.maximize_window()
+
+navegador.get("https://www.go.gov.br/servicos/servico/consultar-veiculo--ipva-multas-e-crlv")
+
+navegador.quit()
