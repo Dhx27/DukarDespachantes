1126304710988c176d58f3889919de0f251d620e
Dhx27 <diogosilvalana27@gmail.com>
2025-01-11 16:14:01 -0300

    feat - inserindo rotina de quebra de captcha na emissão do ivpa. fix - corrigindo erros na emissão do licenciamento.

--- src/DetranMatoGrosso/detranMT.py
+++ src/DetranMatoGrosso/detranMT.py
@@ -65,7 +65,7 @@ def selecionar_download_como_pdf_ipva(pasta_saida, placa_atual_ipva):
 
 def selecionar_download_como_pdf_lic(pasta_saida, placa_atual):
     
-    time.sleep(5)
+    time.sleep(15)
     
     pyautogui.hotkey('ctrl', 'p')
     time.sleep(3)
@@ -100,42 +100,7 @@ def selecionar_download_como_pdf_lic(pasta_saida, placa_atual):
     pyautogui.hotkey('ctrl', 'w')
     time.sleep(1.5)
     pyautogui.hotkey('ctrl', 'w')
-
-#Enviar requisição para resolver o captchaa
-def enviar_requisicao_captcha_1(api_key, base64_image):
     
-    url = "http://2captcha.com/in.php"
-    data = {
-        
-        "key": api_key,
-        "method": "base64",
-        "body": base64_image,
-        "json": 1,
-    }
-
-    response = requests.post(url, data=data)   
-    if response.status_code == 200 and "OK|" in response.text:
-        captcha_id = response.text.split('|')[1]
-        return captcha_id
-    else:
-        raise Exception(f"Erro ao enviar captcha: {response.text}")
-    
-# 2. Obter o token de resposta do CAPTCHA
-def obter_resposta_captcha_1(api_key, captcha_id):
-    
-    url = f"http://2captcha.com/res.php?key={api_key}&action=get&id={captcha_id}&json=1"
-    while True:
-        
-        response = requests.get(url).json()
-        if response["status"] == 1:
-            return response["request"]
-        elif response["request"] == "CAPCHA_NOT_READY":
-            print("Captcha ainda não resolvido, aguardando...")
-        else:
-            raise Exception(f"Erro ao obter resposta: {response}")
-        time.sleep(5)
-    
-
 
 # 1. Enviar requisição para resolver o CAPTCHA turnstile
 def enviar_requisicao_captcha(api_key, sitekey, page_url):
@@ -223,14 +188,18 @@ guia_dados = planilha['BASE']
 
 navegador.get("https://www.detran.mt.gov.br/")
 
-tela_atendimento = WebDriverWait(navegador, 100).until(
+try:
+    
+    tela_atendimento = WebDriverWait(navegador, 10).until(
+        EC.visibility_of_element_located((By.CSS_SELECTOR, "#myPopup > a > picture > img"))
+    )
+            
+    botao_fechar = navegador.find_element(By.CSS_SELECTOR, "#myPopup > span")
+    navegador.execute_script("arguments[0].click();", botao_fechar)
 
-    EC.visibility_of_element_located((By.CSS_SELECTOR, "#myPopup > a > picture > img"))
-)
-        
-botao_fechar = navegador.find_element(By.CSS_SELECTOR, "#myPopup > span")
-navegador.execute_script("arguments[0].click();", botao_fechar)
-        
+except (TimeoutException, NoSuchElementException):  
+     pass
+ 
 tela_cookies = WebDriverWait(navegador, 30).until(
     EC.element_to_be_clickable((By.XPATH, '//*[@id="adopt-accept-all-button"]'))
 ).click()
@@ -245,180 +214,280 @@ linhas = list(guia_dados.iter_rows(min_row=2, max_row=guia_dados.max_row))
 linhaPlan = 1
 
 
-while index < len(linhas):
-    linhaPlan += 1
+try:
 
-    row = linhas[index]
+    while index < len(linhas):
+        linhaPlan += 1
 
-    #Obtem todos os valores dispobniveis na planilha
-    placa_atual = row[0].value
-    renavam_atual = row[1].value
-    cnpj_atual = row[2].value
-    status_lic = row[3].value
+        row = linhas[index]
 
-    if status_lic is None:
-        
-        campo_placa_renavam = WebDriverWait(navegador, 100).until(
-            EC.visibility_of_element_located((By.XPATH, '/html/body/div[1]/div[4]/section/div/div/div/div/div[1]/section/div/div/div/div/div/div[2]/div/div/section/div/div[2]/div/div/div/div/div'))
-        )
-        
-        campo_placa = navegador.find_element(By.CSS_SELECTOR, "#input_placa")
-        campo_placa.clear()
-        campo_placa.send_keys(placa_atual)
-        
-        time.sleep(1.5)
-        
-        campo_renavam = navegador.find_element(By.CSS_SELECTOR, "#input_renavam")
-        campo_renavam.clear()
-        campo_renavam.send_keys(renavam_atual)
-        
-        botao_consultar = WebDriverWait(navegador, 30).until(
-            EC.element_to_be_clickable((By.CSS_SELECTOR, "#formVeiculo > div:nth-child(4) > input.dtrn-frm-sub.dtrn-vin.text-size-acessibilidade"))
-        ).click()
-        
-        # Pegar todas as guias abertas
-        abas = navegador.window_handles
-        
-        # Alternar para a nova guia (última aberta)
-        navegador.switch_to.window(abas[-1])
-        print("Estamos na nova guia:", navegador.title)
-        
-        tela_cnpj = WebDriverWait(navegador, 60).until(
-            EC.visibility_of_element_located((By.XPATH, '/html/body/center/div/form/table/tbody/tr[3]/td[2]/input'))
-        )
+        #Obtem todos os valores dispobniveis na planilha
+        placa_atual = row[0].value
+        renavam_atual = row[1].value
+        cnpj_atual = row[2].value
+        status_lic = row[3].value
 
-        time.sleep(5)
-        
-        try:
-            print("Enviando requisição para resolver o CAPTCHA...")
-            captcha_id = enviar_requisicao_captcha(API_KEY, SITEKEY, PAGE_URL)
-            print("Obtendo resposta do CAPTCHA...")
-            token = obter_resposta_captcha(API_KEY, captcha_id)
-            print("Solução do CAPTCHA obtida:")
-            inserir_token(navegador, token)  # Descomente ao integrar com Selenium
+        if status_lic is None:
             
-            cnpj = cnpj_atual.replace(".", "").replace("/", "").replace("-", "")
-            
-            campo_cnpj = navegador.find_element(By.XPATH, '/html/body/center/div/form/table/tbody/tr[3]/td[2]/input')
-            campo_cnpj.send_keys(cnpj)
-            
-            botao_consultar_veiculo = WebDriverWait(navegador, 60).until(
-                EC.element_to_be_clickable((By.XPATH, '/html/body/center/div/form/table/tbody/tr[4]/td/input'))
-            ).click()
-            
-            tela_baixar_boleto = WebDriverWait(navegador, 60).until(
-                EC.visibility_of_element_located((By.CSS_SELECTOR, "#td_meio_Integral"))
+            campo_placa_renavam = WebDriverWait(navegador, 100).until(
+                EC.visibility_of_element_located((By.XPATH, '/html/body/div[1]/div[4]/section/div/div/div/div/div[1]/section/div/div/div/div/div/div[2]/div/div/section/div/div[2]/div/div/div/div/div'))
             )
             
-            modal_licenciamento = navegador.find_element(By.CSS_SELECTOR, "#cmbTipoDebito")
-            modal_licenciamento.click()
+            campo_placa = navegador.find_element(By.CSS_SELECTOR, "#input_placa")
+            campo_placa.clear()
+            campo_placa.send_keys(placa_atual)
             
-            botao_download_guia = WebDriverWait(navegador, 60).until(
-                EC.element_to_be_clickable((By.CSS_SELECTOR, "#spanDAR_LicenciamentoExercicio"))
+            time.sleep(1.5)
+            
+            campo_renavam = navegador.find_element(By.CSS_SELECTOR, "#input_renavam")
+            campo_renavam.clear()
+            campo_renavam.send_keys(renavam_atual)
+            
+            botao_consultar = WebDriverWait(navegador, 30).until(
+                EC.element_to_be_clickable((By.CSS_SELECTOR, "#formVeiculo > div:nth-child(4) > input.dtrn-frm-sub.dtrn-vin.text-size-acessibilidade"))
             ).click()
             
-            selecionar_download_como_pdf_lic(pasta_saida, placa_atual)
+            # Pegar todas as guias abertas
+            abas = navegador.window_handles
             
-            guia_dados[f'D{linhaPlan}'] = "OK!"  
-            planilha.save(caminho_planilha)
+            # Alternar para a nova guia (última aberta)
+            navegador.switch_to.window(abas[-1])
+            print("Estamos na nova guia:", navegador.title)
             
-            # Voltar para a guia original
-            navegador.switch_to.window(abas[0])
-            print("Voltamos para a guia original:", navegador.title)
+            tela_cnpj = WebDriverWait(navegador, 60).until(
+                EC.visibility_of_element_located((By.XPATH, '/html/body/center/div/form/table/tbody/tr[3]/td[2]/input'))
+            )
+
+            time.sleep(5)
             
+            try:
+                print("Enviando requisição para resolver o CAPTCHA...")
+                captcha_id = enviar_requisicao_captcha(API_KEY, SITEKEY, PAGE_URL)
+                print("Obtendo resposta do CAPTCHA...")
+                token = obter_resposta_captcha(API_KEY, captcha_id)
+                print("Solução do CAPTCHA obtida:")
+                inserir_token(navegador, token)  # Descomente ao integrar com Selenium
+                
+                cnpj = cnpj_atual.replace(".", "").replace("/", "").replace("-", "")
+                
+                campo_cnpj = navegador.find_element(By.XPATH, '/html/body/center/div/form/table/tbody/tr[3]/td[2]/input')
+                campo_cnpj.send_keys(cnpj)
+                
+                botao_consultar_veiculo = WebDriverWait(navegador, 60).until(
+                    EC.element_to_be_clickable((By.XPATH, '/html/body/center/div/form/table/tbody/tr[4]/td/input'))
+                ).click()
+                
+                tela_baixar_boleto = WebDriverWait(navegador, 60).until(
+                    EC.visibility_of_element_located((By.CSS_SELECTOR, "#exibir_servicos_Debitos"))
+                )
+                
+                #modal_licenciamento = navegador.find_element(By.CSS_SELECTOR, "#cmbTipoDebito")
+                #modal_licenciamento.click()
+                
+                elemento_tabela = navegador.find_element(By.CSS_SELECTOR, "#div_servicos_Debitos > table > tbody > tr > td")
+                campo_debitos = elemento_tabela.text
+                
+                if campo_debitos == "Nenhum débito em aberto cadastrado para este veículo.":
+                    guia_dados[f'D{linhaPlan}'] = campo_debitos
+                    planilha.save(caminho_planilha)
+                    index += 1
+                    
+                    navegador.close()
+                    
+                    # Voltar para a guia original
+                    navegador.switch_to.window(abas[0])
+                    print("Voltamos para a guia original:", navegador.title)
+                    continue
+
+                    
+                
+                botao_download_guia = WebDriverWait(navegador, 60).until(
+                    EC.element_to_be_clickable((By.CSS_SELECTOR, "#spanDAR_LicenciamentoExercicio"))
+                ).click()
+                    
+                time.sleep(5)
+                
+                selecionar_download_como_pdf_lic(pasta_saida, placa_atual)
+                
+                guia_dados[f'D{linhaPlan}'] = "OK!"  
+                planilha.save(caminho_planilha)
+                
+                # Voltar para a guia original
+                navegador.switch_to.window(abas[0])
+                print("Voltamos para a guia original:", navegador.title)
+                
+                index += 1
+                
+            except Exception as e:
+                print(f"Erro: {str(e)}")
+        else:
             index += 1
-            
-        except Exception as e:
-            print(f"Erro: {str(e)}")
-    else:
-        index += 1
-        continue
+            continue
+
+except TimeoutException:
+    print("Renicie a automação")
+    breakpoint()
+
 
 
 #                                       EMISSÃO IPVA   
+
+#Enviar requisição para resolver o captcha do ipva
+def enviar_requisicao_captcha_1(api_key, base64_image):
+    
+    url = "http://2captcha.com/in.php"
+    data = {
+        "key": api_key,
+        "method": "base64",
+        "body": base64_image,
+        "json": 1,
+    }
+
+    response = requests.post(url, data=data).json()
+    if response["status"] == 1:  # Status 1 indica sucesso
+        captcha_id = response["request"]
+        return captcha_id
+    else:
+        raise Exception(f"Erro ao enviar captcha: {response}")
+    
+# 2. Obter o token de resposta do CAPTCHA
+def obter_resposta_captcha_1(api_key, captcha_id):
+    
+    url = f"http://2captcha.com/res.php?key={api_key}&action=get&id={captcha_id}&json=1"
+    while True:
+        
+        response = requests.get(url).json()
+        if response["status"] == 1:
+            return response["request"]
+        elif response["request"] == "CAPCHA_NOT_READY":
+            print("Captcha ainda não resolvido, aguardando...")
+        else:
+            raise Exception(f"Erro ao obter resposta: {response}")
+        time.sleep(5)
  
+ 
+def quebra_captcha():
+    
+    try:
+        # Esperar e localizar a imagem do CAPTCHA
+        imagem_captcha = WebDriverWait(navegador, 10).until(
+            EC.visibility_of_element_located((By.CSS_SELECTOR, "body > div > p:nth-child(2) > img:nth-child(1)"))
+        )
+
+        # Obter a URL base64 da imagem do CAPTCHA
+        base64_url = imagem_captcha.get_attribute("src")
+        base64_data = base64_url.split(",")[1]
+
+        # Enviar CAPTCHA para o 2Captcha
+        captcha_id = enviar_requisicao_captcha_1(API_KEY, base64_data)
+
+        # Obter a resposta do CAPTCHA
+        resposta_captcha = obter_resposta_captcha_1(API_KEY, captcha_id)
+        print(f"Resposta do CAPTCHA: {resposta_captcha}")
+
+        # Inserir a resposta no campo do CAPTCHA
+        campo_captcha = WebDriverWait(navegador, 10).until(
+            EC.visibility_of_element_located((By.CSS_SELECTOR, "#ans"))
+        )
+        campo_captcha.clear()
+        campo_captcha.send_keys(resposta_captcha)
+
+        # Submeter o formulário
+        botao_confirmar = WebDriverWait(navegador, 10).until(
+            EC.element_to_be_clickable((By.CSS_SELECTOR, "#jar"))
+        )
+        botao_confirmar.click()
+
+    except (TimeoutException, NoSuchElementException) as e:
+        pass
+        
+        
 navegador.get("https://www.sefaz.mt.gov.br/ipva/emissaoguia/emitir")
 
+quebra_captcha()
 
 index = 0
 linhas = list(guia_dados.iter_rows(min_row=2, max_row=guia_dados.max_row))
 linhaPlan = 1
 
-while index < len(linhas):
-    linhaPlan += 1
+try:
+    while index < len(linhas):
+        linhaPlan += 1
 
-    row = linhas[index]
-    
-    placa_atual_ipva = row[0].value
-    renavam_atual_ipva = row[1].value
-    status_atual_ipva = row[4].value
-    
-    if status_atual_ipva is None:
+        row = linhas[index]
         
-        tela_pesquisa_ipva = WebDriverWait(navegador, 60).until(
-            EC.visibility_of_element_located((By.XPATH, '/html/body/center/form/table/tbody/tr[5]/td[2]/input'))
-        )
+        placa_atual_ipva = row[0].value
+        renavam_atual_ipva = row[1].value
+        status_atual_ipva = row[4].value
         
-        campo_input_renavam = navegador.find_element(By.XPATH, '/html/body/center/form/table/tbody/tr[5]/td[2]/input')
-        campo_input_renavam.clear()
-        campo_input_renavam.send_keys(renavam_atual_ipva)
-        
-        botao_consultar = WebDriverWait(navegador, 60).until(
-            EC.element_to_be_clickable((By.XPATH, '/html/body/center/form/p/input[1]'))
-        ).click()
-        
-        #Aqui pedi captcha
-        
-        #navegador.refresh()
-        
-        tela_ipva = WebDriverWait(navegador, 60).until(
-            EC.visibility_of_element_located((By.CSS_SELECTOR, "table.SEFAZ-TABLE-Moldura"))
-        )
-        
-        elemento_desconto5 = WebDriverWait(navegador, 60).until(
-            EC.element_to_be_clickable((By.CSS_SELECTOR, "#tipoGuia\.2025\.2"))
-        ).click()
-        
-        botao_gerar_guias = WebDriverWait(navegador, 60).until(
-            EC.element_to_be_clickable((By.CSS_SELECTOR, "#btnOK"))
-        ).click()
-        
-        try:
-            imagem_captcha = WebDriverWait(navegador,10).until(
-                EC.visibility_of_element_located((By.CSS_SELECTOR, "body > div > p:nth-child(2) > img:nth-child(1)"))
+        if status_atual_ipva is None:
+            
+            tela_pesquisa_ipva = WebDriverWait(navegador, 60).until(
+                EC.visibility_of_element_located((By.XPATH, '/html/body/center/form/table/tbody/tr[5]/td[2]/input'))
             )
             
-            # URL em formato base64
-            base64_url = imagem_captcha.get_attribute("src")
-
-            # Remover o prefixo 'data:image/png;base64,' e decodificar
-            base64_data = base64_url.split(",")[1]
-            image_data = base64.b64decode(base64_data)
+            campo_input_renavam = navegador.find_element(By.XPATH, '/html/body/center/form/table/tbody/tr[5]/td[2]/input')
+            campo_input_renavam.clear()
+            campo_input_renavam.send_keys(renavam_atual_ipva)
             
-            #Salvar a imagem como um arquivo local
-            with open("captcha.png", "wb") as file:
-                file.write(image_data)
-
-            print("Imagem do CAPTCHA salva como 'captcha.png'")
+            botao_consultar = WebDriverWait(navegador, 60).until(
+                EC.element_to_be_clickable((By.XPATH, '/html/body/center/form/p/input[1]'))
+            ).click()
             
-            # Abrir o arquivo da imagem
-            with open("captcha.png", "rb") as image_file:
-                base64_image = base64.b64encode(image_file.read()).decode("utf-8")
+            #Aqui pedi captch
+            quebra_captcha()
+            
+            tela_ipva = WebDriverWait(navegador, 60).until(
+                EC.visibility_of_element_located((By.CSS_SELECTOR, "table.SEFAZ-TABLE-Moldura"))
+            )
+            
+            elemento_desconto5 = WebDriverWait(navegador, 60).until(
+                EC.element_to_be_clickable((By.CSS_SELECTOR, "#tipoGuia\.2025\.2"))
+            ).click()
+            
+            try:
+                
+                campo_email = WebDriverWait(navegador, 10).until(
+                    EC.visibility_of_element_located((By.CSS_SELECTOR, "#email"))
+                ).send_keys(os.getenv("email_dukar"))
+                
+                campo_numero = WebDriverWait(navegador, 10).until(
+                    EC.visibility_of_element_located((By.CSS_SELECTOR, "#celular"))
+                ).send_keys(os.getenv("numero_dukar"))
+                
+            except (NoSuchElementException, TimeoutException):
+                pass
+            
+            botao_gerar_guias = WebDriverWait(navegador, 60).until(
+                EC.element_to_be_clickable((By.CSS_SELECTOR, "#btnOK"))
+            ).click()
+            
+            try:
+                quebra_captcha() 
+                
+            except(NoSuchElementException, TimeoutException):
+                pass
+            
+            quebra_captcha()
+                        
+            time.sleep(5)
+            
+            selecionar_download_como_pdf_ipva(pasta_saida, placa_atual_ipva)
+            
+            guia_dados[f'E{linhaPlan}'] = "OK!"  
+            planilha.save(caminho_planilha)
+            
+            index +=1
+            
+            navegador.get("https://www.sefaz.mt.gov.br/ipva/emissaoguia/emitir")            
+            
+        else:
+            
+            index += 1 
+            continue
         
-        except (TimeoutException, NoSuchElementException):
-            pass
-        
-        time.sleep(5)
-        
-        selecionar_download_como_pdf_ipva(pasta_saida, placa_atual_ipva)
-        
-        navegador.get("https://www.sefaz.mt.gov.br/ipva/emissaoguia/emitir")            
-        
-    else:
-        
-        index += 1 
-        continue
-    
-
+except TimeoutException:
+    print("Renicie a automação")
+    breakpoint()
 
 navegador.quit()
