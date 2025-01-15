2c725c59529de7659d0b4f97c1edc3b4a29c8d28
Dhx27 <diogosilvalana27@gmail.com>
2025-01-15 11:31:03 -0300

    feat - contrução da rotina de emissão de IPVA e manipulação dos boletos

--- src/DetranDF/detranDF.py
+++ src/DetranDF/detranDF.py
@@ -16,6 +16,7 @@ import os
 import pyautogui
 import pdfplumber
 import re
+from PyPDF2 import PdfReader, PdfWriter
 
 load_dotenv()
 
@@ -59,7 +60,7 @@ def selecionar_download_como_pdf(pasta_saida, placa_atual, ano_ipva):
     time.sleep(3)
 
     # Define o caminho completo para salvar o arquivo
-    caminho_download = os.path.join(pasta_saida, f"{placa_atual}_{ano_ipva}")
+    caminho_download = os.path.join(pasta_saida, f"LIC {placa_atual}_{ano_ipva}")
     caminho_download = os.path.normpath(caminho_download)  # Normaliza o caminho
 
     # Digitar o caminho de salvamento
@@ -194,6 +195,7 @@ try:
             linhas_ipva = tabela_ipva.find_elements(By.TAG_NAME, "tr")
             numero_linhas_ipva = len(linhas_ipva)
 
+            
             #                                   EMISSÃO LICENCIAMENTO
 
             for cont in range(1,  numero_linhas_ipva):
@@ -227,12 +229,72 @@ try:
                 ).click()
 
             guia_dados[f"C{linhaPlan}"] = "OK!"
-            index +=1
-
+            planilha.save(caminho_planilha)
+            
 
             #                                           EMISSÃO IPVA
 
+            botao_ipva_execicio = WebDriverWait(navegador, 60).until(
+                EC.element_to_be_clickable((By.XPATH, "/html/body/app-root/div/div[1]/app-layout-servicos/app-debitos/app-card-detran/mat-card/mat-card-content/div[2]/app-ipva/div/div[2]/div[1]/p/mat-form-field/div/div[1]/div[3]/mat-select"))
+            ).click()
+             
+            time.sleep(2)
+
+            parant_element = navegador.find_element(By.ID, 'cdk-overlay-1')
+
+            child_elements = parant_element.find_elements(By.CSS_SELECTOR, '[id^="mat-option-"]')
+
+            print(f"{len(child_elements)}")
+
+            for cont in range(1, len(child_elements) + 1):
+                selector_ipva = f"/html/body/div[2]/div[2]/div/div/div/mat-option[{cont}]"
+
+                botao_ano_ipva = navegador.find_element(By.XPATH, selector_ipva)
+                botao_ano_ipva.click()
+
+                time.sleep(3)
+
+                selector_ano_ipva = navegador.find_element(By.CSS_SELECTOR, "div:nth-child(3) > app-ipva > div > div.forms-wrapper-body > div.forms-wrapper-body.forms-wrapper-body_table.pad-botton.ng-star-inserted > table > tbody > tr:nth-child(1) > td.mat-cell.cdk-cell.cdk-column-Ano.mat-column-Ano.ng-star-inserted")
+                texto_ipva = selector_ano_ipva.text 
+
+                if texto_ipva != "2025":
+
+                    tabela_emissao_ipva = WebDriverWait(navegador, 60).until(
+                        EC.visibility_of_element_located((By.CSS_SELECTOR, "div.forms-wrapper-body > div.forms-wrapper-body.forms-wrapper-body_table.pad-botton.ng-star-inserted > table > thead > tr"))
+                    )   
+
+                    botao_emitir_ipva = navegador.find_element(By.CSS_SELECTOR, "div:nth-child(3) > app-ipva > div > div.forms-wrapper-body > div.forms-wrapper-body.forms-wrapper-body_table.pad-botton.ng-star-inserted > table > tbody > tr:nth-child(7) > td.mat-cell.cdk-cell.cdk-column-A--o.mat-column-A--o.ng-star-inserted")  
+                    botao_emitir_ipva.click()
+
+                    time.sleep(8)
+
+                    botao_ipva_execicio = WebDriverWait(navegador, 60).until(
+                        EC.element_to_be_clickable((By.XPATH, "/html/body/app-root/div/div[1]/app-layout-servicos/app-debitos/app-card-detran/mat-card/mat-card-content/div[2]/app-ipva/div/div[2]/div[1]/p/mat-form-field/div/div[1]/div[3]/mat-select"))
+                    ).click()
+
+                    continue
+
+                tabela_emissao_ipva = WebDriverWait(navegador, 60).until(
+                    EC.visibility_of_element_located((By.CSS_SELECTOR, "div.forms-wrapper-body > div.forms-wrapper-body.forms-wrapper-body_table.pad-botton.ng-star-inserted > table > thead > tr"))
+                )
+
+                botao_emitir_ipva = navegador.find_element(By.CSS_SELECTOR, "div:nth-child(3) > app-ipva > div > div.forms-wrapper-body > div.forms-wrapper-body.forms-wrapper-body_table.pad-botton.ng-star-inserted > table > tbody > tr:nth-child(1) > td.mat-cell.cdk-cell.cdk-column-A--o.mat-column-A--o.ng-star-inserted > button")
+                botao_emitir_ipva.click()
+
+                time.sleep(8)
+
+                botao_ipva_execicio = WebDriverWait(navegador, 60).until(
+                    EC.element_to_be_clickable((By.XPATH, "/html/body/app-root/div/div[1]/app-layout-servicos/app-debitos/app-card-detran/mat-card/mat-card-content/div[2]/app-ipva/div/div[2]/div[1]/p/mat-form-field/div/div[1]/div[3]/mat-select"))
+                ).click()
             
+            guia_dados[f"D{linhaPlan}"] = "OK!"
+
+            botao_voltar_pesquisa = WebDriverWait(navegador, 60).until(
+                EC.element_to_be_clickable((By.XPATH, "/html/body/app-root/div/div[1]/app-layout-servicos/app-debitos/app-card-detran/mat-card/mat-card-footer/mat-card-actions/button"))
+            ).click()
+
+        index +=1
+        planilha.save(caminho_planilha)    
 
 except Exception as e :
 
@@ -241,3 +303,104 @@ except Exception as e :
     breakpoint()
 
 navegador.quit()
+
+arquivos_pdfs = [f for f in os.listdir(pasta_saida) if f.endswith('.pdf')]
+
+for arquivo in arquivos_pdfs:
+
+    caminho_pdf = os.path.join(pasta_saida, arquivo)
+
+    with pdfplumber.open(caminho_pdf) as pdf:
+        for num_page, pagina in enumerate(pdf.pages):
+
+            texto_boleto = pagina.extract_text()
+            print(texto_boleto)
+
+            if "IPVA" in texto_boleto:
+
+                corte_ipva_1 = re.split("17.Valor Total -", texto_boleto)
+                valor_ipva = corte_ipva_1[2].replace(" ", "").replace("R$", "")
+
+                corte_placa_1 = re.split("PLACA: ", texto_boleto)
+                corte_placa_2 = re.split(" TIPO:", corte_placa_1[1])
+                placa = corte_placa_2[0]
+
+                corte_ano_ipva_1 = re.split(" IPVA ", texto_boleto)
+                corte_ano_ipva_2 = re.split(" - DETRAN DIGITAL", corte_ano_ipva_1[1])
+                ano_ipva_renomear = corte_ano_ipva_2[0]
+
+                caminho_pdf_saida = os.path.join(pasta_saida, f"IPVA {placa}_{ano_ipva_renomear}.pdf")
+
+                if "bordero" in arquivo:
+                    if not os.path.exists(caminho_pdf_saida):
+                        # Abre o PDF com PyPDF2
+                        leitor_pdf = PdfReader(caminho_pdf)
+                        # Criar um novo PDF para a página atual
+                        escritor_pdf = PdfWriter()
+                        escritor_pdf.add_page(leitor_pdf.pages[num_page])  # Adiciona a página correta
+
+
+                        # Salva o PDF no caminho de saída
+                        with open(caminho_pdf_saida, "wb") as arquivo_pdf_saida:
+                            escritor_pdf.write(arquivo_pdf_saida)
+                            print(f"Página salva no caminho: {caminho_pdf_saida}")
+                            pdf.close()
+                            os.remove(caminho_pdf)
+                    else:
+                        print(f"Arquivo já existe: {caminho_pdf_saida}")
+
+                # Procurar a placa na planilha e preencher dados
+                for linha in range(2, guia_dados.max_row + 1):
+                    celula_placa = guia_dados[f'A{linha}'].value  # Coluna 'A' contém as placas
+                    if celula_placa == placa: 
+                        guia_dados[f'E{linha}'] = valor_ipva  # Coluna 'E': VALOR IPVA
+                        planilha.save(caminho_planilha)
+                        break
+                    else:
+                        print(f"Placa {placa} não encontrada na planilha.")
+                
+                continue
+        
+            corte_placa_1 = re.split("Placa: ", texto_boleto)
+            corte_placa_2 = re.split(" Marca/Mod:", corte_placa_1[1])
+            placa = corte_placa_2[0].replace("-", "")
+
+            corte_valor_licenciamento_1 = re.split("ETC ", texto_boleto)
+            corte_valor_licenciamento_2 = re.split("\nAutenticação Mecânica", corte_valor_licenciamento_1[1])
+            valor_licenciamento = corte_valor_licenciamento_2[0].replace("R$", "").replace(" ", "")
+
+            corte_propietario_1 = re.split("Proprietário: ", texto_boleto)
+            corte_propietario_2 = re.split(" CPF: ", corte_propietario_1[1])
+            propietario = corte_propietario_2[0]
+
+            # Procurar a placa na planilha e preencher dados
+            for linha in range(2, guia_dados.max_row + 1):
+                celula_placa = guia_dados[f'A{linha}'].value  # Coluna 'A' contém as placas
+                if celula_placa == placa:  # Verificar se a placa da planilha corresponde à do boleto
+                    guia_dados[f'I{linha}'] = propietario  # Coluna 'B': PROPRIETARIO
+                    guia_dados[f'G{linha}'] = valor_licenciamento  # Coluna 'D': VALOR LIC
+                    guia_dados[f'J{linha}'] = "BOLETO PROCESSADO"  # Coluna 'F': STATUS BOLETO
+                    print(f"Dados preenchidos para a placa {placa} na linha {linha}.")
+                     # Salvar a planilha
+                    planilha.save(caminho_planilha)
+                    break
+            else:
+                print(f"Placa {placa} não encontrada na planilha.")
+
+for linha in range (2, guia_dados.max_row + 1):
+    
+    celula_ipva = guia_dados[f'F{linha}'].value
+    celula_licenciamento = guia_dados[f'G{linha}'].value
+    
+    # Verifica se algum dos valores é None ou está vazio
+    if not celula_ipva or not celula_licenciamento:
+        continue  # Pula para a próxima iteração se qualquer valor estiver vazio
+    
+    valor_ipva_calc = float(celula_ipva.replace('.', '').replace(',', '.'))
+    valor_licenciamento_cal = float(celula_licenciamento.replace('.', '').replace(',', '.'))
+    
+    valor_total = valor_ipva_calc + valor_licenciamento_cal
+    
+    guia_dados[f'H{linha}'] = valor_total  # Coluna 'H': VALOR 
+    planilha.save(caminho_planilha)
+    
